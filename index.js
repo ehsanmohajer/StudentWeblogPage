fb0772a397f0b7fc9ad321b0294c03d958248a06
ehsanmohajer066 <ehsanmohajer066@gmail.com>
2024-10-20 23:16:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hi");
+/**
+ * @license MIT
+ * @copyright 2024 EMK
+ */
+
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 4000 http://localhost:3000");
+});
